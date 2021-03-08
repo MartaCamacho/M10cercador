0e58facd5040d05399e324e351d2bfd5270c6700
martacamacho <martacamachovargas@gmail.com>
2021-03-09 00:41:15 +0100

    buscador in progress

--- src/scripts/SearchFilm.js
+++ src/scripts/SearchFilm.js
@@ -1,12 +1,31 @@
-import { mapActions, mapState, mapMutations } from 'vuex';
+import { mapActions, mapState, mapMutations } from "vuex";
 
 export default {
   name: "SearchFilm",
   computed: {
-      ...mapState(['movies'])
+      ...mapState(["movies", "searchInput", "available", "filteredMovies"]),
+      available: {
+          get() {
+              return this.$store.state.available
+          },
+          set(value) {
+              this.$store.commit("setAvailable", value)
+          }
+      },
+      searchInput: {
+        get() {
+            return this.$store.state.searchInput;
+        },
+        set(value) {
+            this.$store.commit("setSearch", value)
+        }
+    },
   },
   methods: {
-      ...mapActions(['getMovies']),
-      ...mapMutations(['setMovies'])
+      ...mapActions(["getMovies", "getAvailable", "getFiltered"]),
+      ...mapMutations(["setMovies", "setAvailable", "sortedMovies", "searchResult"])
+  },
+  mounted: async function(){
+    await this.$store.dispatch('getMovies');
   }
 };
--- src/store.js
+++ src/store.js
@@ -6,25 +6,63 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     movies: [],
+    searchInput: "",
+    available: 'all',
+    filteredMovies: []
   },
   mutations: {
-    setMovies: function (state, moviesAction) {
+    setMovies: function(state, moviesAction) {
       state.movies = moviesAction;
     },
+    setAvailable: function(state, boolean) {
+        state.available = boolean;
+    },
+    setSearch: function(state, value) {
+        state.searchInput = value;
+    },
+    sortedMovies: function(state, value) {
+        let tempMovies = state.movies;
+        if (value == "all") {
+        state.filteredMovies = tempMovies;
+        } else if( value === true) {
+        tempMovies = tempMovies.filter((item) => {
+        return (item.available === true)
+        })
+        state.filteredMovies = tempMovies;
+        } else if( value === false) {
+        tempMovies = tempMovies.filter((item) => {
+        return (item.available === false)
+        })
+        state.filteredMovies = tempMovies;
+        }
+        console.log(state.filteredMovies)
+    },
+    searchResult: function(state, value) {
+        let tempMovies = state.movies
+        
+        if (value != '' && value) {
+            tempMovies = tempMovies.filter((item) => {
+                return item.title
+                    .toUpperCase()
+                    .includes(value.toUpperCase())
+                })
+            }
+        state.filteredMovies = tempMovies;
+    }
   },
   actions: {
     getMovies: async function({ commit }) {
-      const data = await fetch('/assets/movies.json', {
-        headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-         }
-       });
+      const data = await fetch("/assets/movies.json");
       const theMovies = await data.json();
-      console.log(theMovies)
-      commit('setMovies', theMovies);
+      commit("setMovies", theMovies);
     },
-  },
+    getAvailable: async function({ commit }, value) {
+        commit("setAvailable", value);
+    },
+    getFiltered: async function({ commit }, value) {
+        commit("searchResult", value);
+    },
+  }
 });
 
 export default store;
